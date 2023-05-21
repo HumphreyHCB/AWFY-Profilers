e9852e5dfad7150eaaaed44213cd54aaba8425a5
Humphrey Burchell <humphreyburchell@gmail.com>
2023-05-21 19:05:39 +0100

    added more profiler and xml processing

--- Readers/ProfilerDumpsReader.java
+++ Readers/ProfilerDumpsReader.java
@@ -5,10 +5,24 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-import javax.naming.spi.DirStateFactory.Result;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.xml.sax.SAXException;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -20,16 +34,40 @@ import java.util.Comparator;
 
 public class ProfilerDumpsReader {
     public static void main(String[] args) {
-        proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump", "Readers/JSONDumps/report3/output21.json");
-        proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump2", "Readers/JSONDumps/report3/output22.json");
-        proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump3", "Readers/JSONDumps/report3/output23.json");
-        proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump4", "Readers/JSONDumps/report3/output24.json");
-        proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump5", "Readers/JSONDumps/report3/output25.json");
-        proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump6", "Readers/JSONDumps/report3/output26.json");
-        proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump7", "Readers/JSONDumps/report3/output27.json");
-        proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump8", "Readers/JSONDumps/report3/output28.json");
-        proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump9", "Readers/JSONDumps/report3/output29.json");
-        proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump10", "Readers/JSONDumps/report3/output30.json");
+        proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump2", "output500.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/A/ProfilesDump", "Readers/JSONDumps/report3/output.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/A/ProfilesDump2", "Readers/JSONDumps/report3/output2.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/A/ProfilesDump3", "Readers/JSONDumps/report3/output3.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/A/ProfilesDump4", "Readers/JSONDumps/report3/output4.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/A/ProfilesDump5", "Readers/JSONDumps/report3/output5.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/A/ProfilesDump6", "Readers/JSONDumps/report3/output6.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/A/ProfilesDump7", "Readers/JSONDumps/report3/output7.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/A/ProfilesDump8", "Readers/JSONDumps/report3/output8.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/A/ProfilesDump9", "Readers/JSONDumps/report3/output9.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/A/ProfilesDump10", "Readers/JSONDumps/report3/output10.json");
+
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/B/ProfilesDump", "Readers/JSONDumps/report3/output11.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/B/ProfilesDump2", "Readers/JSONDumps/report3/output12.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/B/ProfilesDump3", "Readers/JSONDumps/report3/output13.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/B/ProfilesDump4", "Readers/JSONDumps/report3/output14.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/B/ProfilesDump5", "Readers/JSONDumps/report3/output15.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/B/ProfilesDump6", "Readers/JSONDumps/report3/output16.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/B/ProfilesDump7", "Readers/JSONDumps/report3/output17.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/B/ProfilesDump8", "Readers/JSONDumps/report3/output18.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/B/ProfilesDump9", "Readers/JSONDumps/report3/output19.json");
+        // proccesDumpDir("/home/hburchell/Repos/DeepStorage/Report3/B/ProfilesDump10", "Readers/JSONDumps/report3/output20.json");
+        
+
+        // proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump", "Readers/JSONDumps/report3/output21.json");
+        // proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump2", "Readers/JSONDumps/report3/output22.json");
+        // proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump3", "Readers/JSONDumps/report3/output23.json");
+        // proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump4", "Readers/JSONDumps/report3/output24.json");
+        // proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump5", "Readers/JSONDumps/report3/output25.json");
+        // proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump6", "Readers/JSONDumps/report3/output26.json");
+        // proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump7", "Readers/JSONDumps/report3/output27.json");
+        // proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump8", "Readers/JSONDumps/report3/output28.json");
+        // proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump9", "Readers/JSONDumps/report3/output29.json");
+        // proccesDumpDir("/home/hburchell/Repos/AWFY-Profilers/ProfilesDump10", "Readers/JSONDumps/report3/output30.json");
 
     }
 
@@ -42,10 +80,10 @@ public class ProfilerDumpsReader {
             if (ProfilerDump[0].contains("AsyncDumps")) {
                 System.out.println("---------------AsyncDumps---------------");
                 JSONObject AsyncDumps = new JSONObject();
-                for (int i = 0; i < ProfilerDump.length - 1; i++) {
+                for (int i = 0; i < ProfilerDump.length; i++) {
                     JSONObject benchmark = new JSONObject();
-                    String[][] top5 = AsyncHottestMethods(ProfilerDump[i]);
-                    for (String[] strings : top5) {
+                    String[][] top10 = AsyncHottestMethods(ProfilerDump[i]);
+                    for (String[] strings : top10) {
                         System.out.println(strings[0] + " " + strings[1]);
                         benchmark.put(strings[0], strings[1]);
                     }
@@ -58,10 +96,10 @@ public class ProfilerDumpsReader {
             } else if (ProfilerDump[0].contains("HonestProfiler")) {
                 System.out.println("---------------HonestProfiler---------------");
                 JSONObject HonestProfiler = new JSONObject();
-                for (int i = 1; i < ProfilerDump.length - 1; i++) {
+                for (int i = 0; i < ProfilerDump.length; i++) {
                     JSONObject benchmark = new JSONObject();
-                    String[][] top5 = HonestProfilerHottestMethods(ProfilerDump[i]);
-                    for (String[] strings : top5) {
+                    String[][] top10 = HonestProfilerHottestMethods(ProfilerDump[i]);
+                    for (String[] strings : top10) {
                         System.out.println(strings[0] + " " + strings[1]);
                         benchmark.put(strings[0], strings[1]);
                     }
@@ -73,10 +111,10 @@ public class ProfilerDumpsReader {
             } else if (ProfilerDump[0].contains("JavaFlightRecorder")) {
                 System.out.println("---------------JavaFlightRecorder---------------");
                 JSONObject JavaFlightRecorder = new JSONObject();
-                for (int i = 1; i < ProfilerDump.length - 1; i++) {
+                for (int i = 0; i < ProfilerDump.length; i++) {
                     JSONObject benchmark = new JSONObject();
-                    String[][] top5 = JFRHottestMethods(ProfilerDump[i]);
-                    for (String[] strings : top5) {
+                    String[][] top10 = JFRHottestMethods(ProfilerDump[i]);
+                    for (String[] strings : top10) {
                         System.out.println(strings[0] + " " + strings[1]);
                         if(strings[0] == null)
                         {
@@ -93,10 +131,10 @@ public class ProfilerDumpsReader {
             } else if (ProfilerDump[0].contains("Perf")) {
                 System.out.println("---------------Perf---------------");
                 JSONObject Perf = new JSONObject();
-                for (int i = 1; i < ProfilerDump.length - 1; i++) {
+                for (int i = 0; i < ProfilerDump.length; i++) {
                     JSONObject benchmark = new JSONObject();
-                    String[][] top5 = PerfHottestMethods(ProfilerDump[i]);
-                    for (String[] strings : top5) {
+                    String[][] top10 = PerfHottestMethods(ProfilerDump[i]);
+                    for (String[] strings : top10) {
                         System.out.println(strings[0] + " " + strings[1]);
                         benchmark.put(strings[0], strings[1]);
                     }
@@ -104,6 +142,26 @@ public class ProfilerDumpsReader {
                     System.out.println("");
                 }
                 AllProfilerDumps.put("Perf", Perf);
+            } else if (ProfilerDump[0].contains("JProfiler")) {
+                System.out.println("---------------JProfiler---------------");
+                JSONObject JProfiler = new JSONObject();
+                for (int i = 0; i < ProfilerDump.length; i++) {
+                    JSONObject benchmark = new JSONObject();
+                    String[][] top10 = JProfilerHottestMethods(ProfilerDump[i]);
+                    for (String[] strings : top10) {
+                        System.out.println(strings[0] + " " + strings[1]);
+                        if(strings[0] == null)
+                        {
+                            benchmark.put(" ", "0");
+                        }
+                        else{
+                        benchmark.put(strings[0], strings[1]);
+                        }
+                    }
+                    JProfiler.put(new File(ProfilerDump[i]).getName(), benchmark);
+                    System.out.println("");
+                }
+                AllProfilerDumps.put("JProfiler", JProfiler);
             }
 
         }
@@ -133,8 +191,13 @@ public class ProfilerDumpsReader {
                         DumpFiles[i][j] = currentDir[j].getAbsolutePath();
                     }
                 }
-                Arrays.sort(DumpFiles[i]);
-
+                if (DumpFiles[i][13] == null) {
+                    
+                }
+                else{
+                    Arrays.sort(DumpFiles[i]);
+                }
+              
             }
         }
         Arrays.sort(DumpFiles, new Comparator<String[]>() {
@@ -164,7 +227,7 @@ public class ProfilerDumpsReader {
     }
 
     private static String[][] AsyncHottestMethods(String Filename) {
-        String[][] top5 = new String[10][2];
+        String[][] top10 = new String[10][2];
         try {
             File myObj = new File(Filename);
             Scanner myReader = new Scanner(myObj);
@@ -173,11 +236,11 @@ public class ProfilerDumpsReader {
                     for (int i = 0; i < 10; i++) {
                         String[] split = myReader.nextLine().split("\\s+");
                         if (split[0] == "") {
-                            top5[i][0] = split[4];
-                            top5[i][1] = split[2];
+                            top10[i][0] = split[4];
+                            top10[i][1] = split[2];
                         } else {
-                            top5[i][0] = split[3];
-                            top5[i][1] = split[1];
+                            top10[i][0] = split[3];
+                            top10[i][1] = split[1];
                         }
                     }
                     break;
@@ -189,11 +252,11 @@ public class ProfilerDumpsReader {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        return top5;
+        return top10;
     }
 
     private static String[][] HonestProfilerHottestMethods(String Filename) {
-        String[][] top5 = new String[10][2];
+        String[][] top10 = new String[10][2];
         ProcessBuilder processBuilder = new ProcessBuilder();
         try {
             processBuilder.command("/home/hburchell/Repos/AWFY-Profilers/Profilers/honest-profiler/console", "-log",
@@ -206,8 +269,8 @@ public class ProfilerDumpsReader {
                 if (line.equals("Flat Profile (by method):")) {
                     for (int i = 0; i < 10; i++) {
                         String[] split = reader.readLine().split("\\s+", 5);
-                        top5[i][0] = split[4];
-                        top5[i][1] = split[3].substring(0, split[3].length() - 1);
+                        top10[i][0] = split[4];
+                        top10[i][1] = split[3].substring(0, split[3].length() - 1);
                     }
                     break;
                 }
@@ -217,7 +280,7 @@ public class ProfilerDumpsReader {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        return top5;
+        return top10;
     }
 
     // java JfrTop
@@ -225,15 +288,15 @@ public class ProfilerDumpsReader {
     // "" 100
 
     private static String[][] JFRHottestMethods(String Filename) {
-        String[][] top5 = new JfrTop().getHottestMethods(Filename);
-        return top5;
+        String[][] top10 = new JfrTop().getHottestMethods(Filename);
+        return top10;
     }
 
     private static String[][] PerfHottestMethods(String Filename) {
-        String[][] top5 = new String[5][2];
+        String[][] top10 = new String[10][2];
         ProcessBuilder processBuilder = new ProcessBuilder();
         try {
-            processBuilder.command("sudo", "perf", "report", "--stdio", "-i", Filename);
+            processBuilder.command("perf", "report", "--stdio", "-i", Filename);
             Process process = processBuilder.start();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line = reader.readLine();
@@ -244,8 +307,8 @@ public class ProfilerDumpsReader {
                     reader.readLine();
                     for (int i = 0; i < 5; i++) {
                         String[] split = reader.readLine().split("\\s+", 7);
-                        top5[i][0] = split[split.length - 1];
-                        top5[i][1] = split[1];
+                        top10[i][0] = split[split.length - 1];
+                        top10[i][1] = split[1];
                     }
                     break;
                 }
@@ -255,7 +318,65 @@ public class ProfilerDumpsReader {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        return top5;
+        return top10;
+    }
+
+    private static String[][] JProfilerHottestMethods(String Filename) {
+        String[][] top10 = new String[10][2];
+        // most of this is riped from stack overflow
+        try {
+            File fXmlFile = new File(Filename);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("hotspot");
+            NodeList tree = doc.getElementsByTagName("tree");
+            int lengtha = nList.getLength();
+        
+            for (int temp = 0; temp < 10 && temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    top10[temp][0]=eElement.getAttribute("class");
+                    top10[temp][1]=eElement.getAttribute("percent");
+                }
+            }
+            } catch (Exception e) {
+            e.printStackTrace();
+            }
+        return top10;
+    }
+
+    private static String[][] YourKitHottestMethods(String Filename) {
+        String[][] top10 = new String[10][2];
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        try {
+            processBuilder.command("perf", "report", "--stdio", "-i", Filename);
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = reader.readLine();
+
+            while (line != null) {
+                if (line.startsWith("# Overhead  Command          Shared Object         Symbol ")) {
+                    reader.readLine();
+                    reader.readLine();
+                    for (int i = 0; i < 5; i++) {
+                        String[] split = reader.readLine().split("\\s+", 7);
+                        top10[i][0] = split[split.length - 1];
+                        top10[i][1] = split[1];
+                    }
+                    break;
+                }
+                line = reader.readLine();
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return top10;
     }
 
 }
