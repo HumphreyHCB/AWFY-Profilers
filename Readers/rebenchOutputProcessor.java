801647fa99d83cebd2de4a27df3ab33263402eb6
Humphrey Burchell <humphreyburchell@gmail.com>
2023-06-25 19:19:57 +0100

    added bulk processing

--- Readers/rebenchOutputProcessor.java
+++ Readers/rebenchOutputProcessor.java
@@ -36,38 +36,40 @@ public class rebenchOutputProcessor {
 
         // proccesDataDump("datapoint1.data","Readers/FlagReport.json");
         // proccesDataDump("datapoint2.data","Readers/FlagReport2.json");
-        // proccesDataDump("datapoint3.data","Readers/FlagReport3.json");
+        //proccesDataDump("First1ms.data","Readers/JSONDumps/report5/output.json");
+        // proccesBulkDataDump("BatchRun1ms.data", "Readers/JSONDumps/report5/output2.json", 2);
+        // proccesBulkDataDump("BatchRun1ms.data", "Readers/JSONDumps/report5/output3.json", 3);
 
-        //proccesBulkDataDump("Readers/BatchRun.data", "Readers/FlagReport4.json", 4);
-        //proccesBulkDataDump("Readers/BatchRun.data", "Readers/FlagReport5.json", 5);
+        // proccesBulkDataDump("BatchRun1ms.data", "Readers/JSONDumps/report5/output4.json", 4);
+        // proccesBulkDataDump("BatchRun1ms.data", "Readers/JSONDumps/report5/output5.json", 5);
 
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport6.json",6);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport7.json",7);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport8.json",8);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport9.json",9);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport10.json",10);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output6.json",6);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output7.json",7);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output8.json",8);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output9.json",9);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output10.json",10);
 
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport11.json",11);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport12.json",12);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport13.json",13);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport14.json",14);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport15.json",15);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport16.json",16);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport17.json",17);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport18.json",18);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport19.json",19);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport20.json",20);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output11.json",11);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output12.json",12);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output13.json",13);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output14.json",14);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output15.json",15);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output16.json",16);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output17.json",17);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output18.json",18);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output19.json",19);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output20.json",20);
 
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport21.json",21);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport22.json",22);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport23.json",23);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport24.json",24);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport25.json",25);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport26.json",26);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport27.json",27);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport28.json",28);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport29.json",29);
-        // proccesBulkDataDump("Readers/BatchRun.data","Readers/FlagReport30.json",30);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output21.json",21);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output22.json",22);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output23.json",23);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output24.json",24);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output25.json",25);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output26.json",26);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output27.json",27);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output28.json",28);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output29.json",29);
+        // proccesBulkDataDump("BatchRun1ms.data","Readers/JSONDumps/report5/output30.json",30);
 
         // proccesDataDump("RebenchDump/data.data","Readers/JSONDumps/report4/output.json");
         // proccesDataDump("RebenchDump/data2.data","Readers/JSONDumps/report4/output2.json");
@@ -111,44 +113,81 @@ public class rebenchOutputProcessor {
         String benchmark = "Towers";
 
 
-        getMinAvgMax(processFile("RebenchDump/data.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data2.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data3.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data4.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data5.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data6.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data7.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data8.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data9.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data10.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data2.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data3.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data4.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data5.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data6.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data7.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data8.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data9.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data10.data"), profiler, benchmark);
 
 
-        getMinAvgMax(processFile("RebenchDump/data11.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data12.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data13.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data14.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data15.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data16.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data17.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data18.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data19.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data20.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data11.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data12.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data13.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data14.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data15.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data16.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data17.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data18.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data19.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data20.data"), profiler, benchmark);
 
-        getMinAvgMax(processFile("RebenchDump/data21.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data22.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data23.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data24.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data25.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data26.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data27.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data28.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data29.data"), profiler, benchmark);
-        getMinAvgMax(processFile("RebenchDump/data30.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data21.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data22.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data23.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data24.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data25.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data26.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data27.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data28.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data29.data"), profiler, benchmark);
+        // getMinAvgMax(processFile("RebenchDump/data30.data"), profiler, benchmark);
 
-        System.out.println("" + golbalmin + " " + globaltotal/30  + " " + golbalmax);
+        // System.out.println("" + golbalmin + " " + globaltotal/30  + " " + golbalmax);
 
          HashMap<String,ArrayList<Double>> dataset = new HashMap<String,ArrayList<Double>>();
-        //  dataset = compaireForOverhead(processFile("RebenchDump/data.data"), dataset);
+
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 2), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 3), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 4), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 5), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 6), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 7), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 8), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 9), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 10), dataset);
+
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 11), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 12), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 13), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 14), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 15), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 16), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 17), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 18), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 19), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 20), dataset);
+
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 21), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 22), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 23), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 24), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 25), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 26), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 27), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 28), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 29), dataset);
+         dataset = compaireForOverhead(processSpecificBulkFile("Combind.data", 30), dataset);
+
+
+
+
+
+        //dataset = compaireForOverhead(processFile("RebenchDump/data.data"), dataset);
         // dataset = compaireForOverhead(processFile("RebenchDump/data2.data"), dataset);
         // dataset = compaireForOverhead(processFile("RebenchDump/data3.data"), dataset);
         // dataset = compaireForOverhead(processFile("RebenchDump/data4.data"), dataset);
@@ -215,10 +254,10 @@ public class rebenchOutputProcessor {
         // dataset = (processFile("RebenchDump/data29.data"));
         // dataset = (processFile("RebenchDump/data30.data"));
 
-        // TreeMap<String,Double> overhead = new TreeMap<String,Double>(getaverage(dataset));
-        // for (String key  : overhead.keySet()) {
-        //     System.out.println(key + " " + new DecimalFormat("0.00").format(overhead.get(key)) + "%");
-        // }
+        TreeMap<String,Double> overhead = new TreeMap<String,Double>(getaverage(dataset));
+        for (String key  : overhead.keySet()) {
+            System.out.println(key + " " + new DecimalFormat("0.00").format(overhead.get(key)) + "%");
+        }
     }
 
     public static HashMap<String,ArrayList<Double>> compaireForOverhead( HashMap<String,ArrayList<Double>> profiled, HashMap<String,ArrayList<Double>> dataset) {
@@ -520,6 +559,57 @@ public class rebenchOutputProcessor {
                          var_iteration =  Integer.parseInt(split[10]);
 
                          String key = var_iteration + " " +profiler + " " + benchmark;
+
+                         if (map.containsKey(key)) {
+                            map.get(key).add(exeTime);
+                            //heatMap.put(key, heatMap.get(key).add(i));
+                        }
+                        else{
+                            ArrayList<Double> array = new ArrayList<Double>();
+                            array.add(exeTime);
+                            map.put(key, array );
+                        }
+                    }
+                    
+                }
+             
+            }
+            myReader.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+        return map;
+    }
+
+    private static  HashMap<String,ArrayList<Double>> processSpecificBulkFile(String Filename, int Specificiteration) {
+        HashMap<String,ArrayList<Double>> map = new  HashMap<String,ArrayList<Double>>();
+
+        try {
+            File myObj = new File(Filename);
+            Scanner myReader = new Scanner(myObj);
+
+            int iteration = 0;
+            Double exeTime = 0.0;
+            String benchmark = "";
+            String profiler = "";
+            int var_iteration = 0;
+            while (myReader.hasNextLine()) {
+                if (myReader.nextLine().startsWith("# Source:")) {
+                    while (myReader.hasNextLine()) {
+                        String[] split = myReader.nextLine().split("\\s+");
+                         iteration = Integer.parseInt(split[1]);
+                         exeTime = Double.parseDouble(split[2]);
+                         benchmark = split[5];
+                         profiler = split[7];
+                         var_iteration =  Integer.parseInt(split[10]);
+                        if(var_iteration == Specificiteration){
+
+                        }
+                        else{
+                            continue;
+                        }
+                         String key = profiler + " " + benchmark;
 
                          if (map.containsKey(key)) {
                             map.get(key).add(exeTime);
