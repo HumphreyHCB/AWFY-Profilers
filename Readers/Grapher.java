4164b4d4c18b4c53925e0874406a59d2fb052939
Humphrey Burchell <humphreyburchell@gmail.com>
2023-06-25 19:21:05 +0100

    more stly changes

--- Readers/Grapher.java
+++ Readers/Grapher.java
@@ -1,6 +1,8 @@
+import java.awt.BasicStroke;
 import java.awt.Font;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import org.knowm.xchart.BitmapEncoder;
@@ -9,9 +11,6 @@ import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
 import org.knowm.xchart.BitmapEncoder.BitmapFormat;
-import org.knowm.xchart.VectorGraphicsEncoder.VectorGraphicsFormat;
-import org.knowm.xchart.PdfboxGraphicsEncoder;
-import org.knowm.xchart.VectorGraphicsEncoder;
 import org.knowm.xchart.XYSeries.XYSeriesRenderStyle;
 import org.knowm.xchart.style.Styler.ChartTheme;
 import org.knowm.xchart.style.Styler.LegendPosition;
@@ -27,7 +26,7 @@ public class Grapher {
     public void Graph(String GraphName, ArrayList<Double> xData, ArrayList<Double> yData) {
 
         // Create Chart
-        XYChart chart = new XYChartBuilder().width(1400).height(1000).title(GraphName).xAxisTitle("Median Runtime of Iteration").yAxisTitle("Percentage of Runtime of Method").build();
+        XYChart chart = new XYChartBuilder().width(1400).height(1000).title(GraphName).xAxisTitle("Median Run time of Iteration").yAxisTitle("Percentage of Runtime of Method").build();
 
         // Customize Chart
         chart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Scatter);
@@ -46,35 +45,16 @@ public class Grapher {
 
         XYChart chart = new XYChartBuilder().width(1200).height(1400).title(GraphName).xAxisTitle("Median Run Time of Invocation").yAxisTitle("% of Run Time").theme(ChartTheme.Matlab).build();
 
-        for (String key : xDatas.keySet()) {
-            System.out.println(key);
-            ArrayList<Double> tempx = xDatas.get(key);
-            ArrayList<Double> tempy = yDatas.get(key);
-            for (int i = 0; i < tempx.size(); i++) {
-                System.out.print(tempx.get(i)+" ");
-            }
-            System.out.println();
-            System.out.println();
-            for (int i = 0; i < tempy.size(); i++) {
-                System.out.print(tempy.get(i)+" ");
-            }
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-
-        }
-
         // Customize Chart
         chart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Scatter);
         chart.getStyler().setChartTitleVisible(true);
         chart.getStyler().setLegendPosition(LegendPosition.InsideNE);
-        chart.getStyler().setMarkerSize(8);
+        chart.getStyler().setMarkerSize(12);
         
         chart.getStyler().setAxisTitlePadding(9);
-        chart.getStyler().setAxisTitleFont(new Font("Verdana", Font.PLAIN, 20));
-        chart.getStyler().setChartTitleFont(new Font("Verdana", Font.PLAIN, 20));
-        chart.getStyler().setAxisTickLabelsFont(new Font("Verdana", Font.PLAIN, 12));
+        chart.getStyler().setAxisTitleFont(new Font("Verdana", Font.PLAIN, 28));
+        chart.getStyler().setChartTitleFont(new Font("Verdana", Font.PLAIN, 28));
+        chart.getStyler().setAxisTickLabelsFont(new Font("Verdana", Font.PLAIN, 18));
 
          //chart.getStyler().setAxisTickMarksStroke(BasicStroke.)
 
@@ -85,13 +65,17 @@ public class Grapher {
 
         chart.getStyler().setYAxisMax(100d);
         chart.getStyler().setYAxisMin(0d);
-        chart.getStyler().setXAxisMin(0d);
+        //chart.getStyler().setXAxisMin(0d);
         chart.setTitle(GraphName);
         // Series
         for (String key : xDatas.keySet()) {
             chart.addSeries(key, xDatas.get(key), yDatas.get(key));
+            System.out.print(xDatas.get(key)+ " ");
         }
-        
+        System.out.println();
+        for (String key : xDatas.keySet()) {
+            System.out.print(yDatas.get(key)+ " ");
+        }
         chart.getStyler().setLegendFont(new Font("Verdana", Font.PLAIN, 12));
         new SwingWrapper(chart).displayChart();
 
